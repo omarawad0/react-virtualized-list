e984f10b2f8bcedb196017bca5476d8f4164f209
Omar Awad <omar.awad@emushrif.om>
2024-12-11 18:59:40 +0200

    Add and configure Prettier and Eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,43 @@
+import tseslint from 'typescript-eslint'
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import react from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier/recommended'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    // specify the formats on which to apply the rules below
     files: ['**/*.{ts,tsx}'],
+    // use predefined configs in installed eslint plugins
+    extends: [
+      // js
+      js.configs.recommended,
+      // ts
+      ...tseslint.configs.recommended,
+      // react
+      react.configs.flat.recommended,
+      // import
+      importPlugin.flatConfigs.recommended,
+      // prettier
+      prettier
+    ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    settings: {
+      // for eslint-plugin-react to auto detect react version
+      react: {
+        version: 'detect'
+      }
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
+      // set of custom rules
+      'no-console': 'warn',
+      'react/button-has-type': 'error',
+      'react/react-in-jsx-scope': ['off'],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+    }
+  }
 )
